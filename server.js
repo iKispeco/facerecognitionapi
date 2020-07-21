1291ff40df8458f0fa45e20670c12a1a26c0b61b
iKispeco <igor.kispeco@gmail.com>
2020-07-21 17:00:31 +0200

    upgrading server to heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   	connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-silhouetted-63662',
     user : 'postgres',
     password : '230919877Ik',
     database : 'smart-brain'
